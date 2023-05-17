dae3b4576664c2d7aa1d871f3d8ffde557ab3452
Djibh <julien.riera@protonmail.com>
2023-05-17 22:17:44 +0200

    style(cards): update cards content

--- scripts/recipeDom.js
+++ scripts/recipeDom.js
@@ -21,33 +21,42 @@ export function buildRecipesDOM(recipesList) {
     const recipeCard = document.createElement("article");
     recipeCard.classList.add("card");
     recipeCard.classList.add("recipe-card");
-    const img = document.createElement("img");
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add("card-img-top");
+    const img = document.createElement('img');
     img.setAttribute('src', '../assets/logo.png');
     img.setAttribute('alt', 'Logo du site Les Petits Plats');
     img.classList.add("recipe-card__img");
-    img.classList.add("card-img-top");
-    recipeCard.appendChild(img);
+    imgContainer.appendChild(img);
+
+    recipeCard.appendChild(imgContainer);
+
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('col-8');
     const title = document.createElement("h2");
-    title.classList.add('col', 'recipe-card__title');
+    title.classList.add('recipe-card__title');
     title.innerText = recipe.name;
+    titleContainer.appendChild(title);
 
+    const timeContainer = document.createElement('div');
+    timeContainer.classList.add('col-4', 'recipe-card__duration-container');
     const time = document.createElement("strong");
-    time.classList.add('col', 'recipe-card__duration');
-    // const clock = document.createElement("i");
-    // clock.classList.add("fa-sharp", "fa-regular", "fa-clock");
+    time.classList.add('recipe-card__duration');
+    timeContainer.appendChild(time);
+    
     time.innerText = `${recipe.time} min `;
-    // time.appendChild(clock);
 
     const cardBodyHeader = document.createElement('div');
     cardBodyHeader.classList.add('row');
-    cardBodyHeader.appendChild(title);
-    cardBodyHeader.appendChild(time);
+    cardBodyHeader.appendChild(titleContainer);
+    cardBodyHeader.appendChild(timeContainer);
 
+    const ingredientsListContainer = document.createElement('div');
+    ingredientsListContainer.classList.add('col');
     const ingredientsList = document.createElement("ul");
-    ingredientsList.classList.add('col');
     ingredientsList.classList.add("ingredients-list");
 
     ingredients.forEach((ingredient) => {
@@ -67,14 +76,19 @@ export function buildRecipesDOM(recipesList) {
       ingredientsList.appendChild(ingredientItem);
     });
 
+    ingredientsListContainer.appendChild(ingredientsList);
+
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('col');
     const description = document.createElement("p");
     description.innerText = recipe.description;
-    description.classList.add('col', 'card__description-text');
+    description.classList.add('card__description-text');
+    descriptionContainer.appendChild(description);
 
     const cardBodyContent = document.createElement('div');
     cardBodyContent.classList.add('row');
-    cardBodyContent.appendChild(ingredientsList);
-    cardBodyContent.appendChild(description);
+    cardBodyContent.appendChild(ingredientsListContainer);
+    cardBodyContent.appendChild(descriptionContainer);
 
     cardBsContainer.appendChild(recipeCard);
 
